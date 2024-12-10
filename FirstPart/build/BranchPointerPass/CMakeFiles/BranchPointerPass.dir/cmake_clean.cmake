e781b1b7a36a328eca4217726d8e78da7f7afcf2
Zahra Shiraz <zshiraz@vclvm177-57.vcl.ncsu.edu>
2024-12-09 22:17:32 -0500

    part 1

---
+++ FirstPart/build/BranchPointerPass/CMakeFiles/BranchPointerPass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BranchPointerPass.dir/BranchPointerPass.cpp.o"
+  "CMakeFiles/BranchPointerPass.dir/BranchPointerPass.cpp.o.d"
+  "libBranchPointerPass.pdb"
+  "libBranchPointerPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BranchPointerPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
